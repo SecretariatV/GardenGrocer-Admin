b0b7ecbd1e05c61dbdef5653ca7f7ff8b4f137b9
secretariatv <oliver.b25.f@gmail.com>
2024-09-06 14:31:50 +0900

    create: register logic

--- src/features/layout/header/index.tsx
+++ src/features/layout/header/index.tsx
@@ -1,10 +1,20 @@
 import { HamburgerButton } from "@features/ui";
 import S from "./index.module.scss";
+import { useNavigate } from "react-router-dom";
 
 export const Header = () => {
+  const navigate = useNavigate();
+
+  const handleRegister = () => {
+    navigate("/register");
+  };
+
   return (
     <header className={S.root} role="banner">
       <HamburgerButton />
+      <button type="button" onClick={handleRegister}>
+        Register
+      </button>
     </header>
   );
 };
